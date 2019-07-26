f8b4d33fcf941f81f185452c2bbc30a5e781ae16
Chu Wing Kin <cwksc.person@gmail.com>
2019-07-26 15:54:13 +0800

    Program upload

---
+++ MacroLib/MacroLib/ArrayLoopingMacro.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "SupportingMacro.h"
+
+// 针对遍历阵列，默认 var 从 0 开始，递增为 1
+#define loopBy(var, ...) \
+    for(int var = 0; var != __VA_ARGS__; var++)
+
+// 不在乎循环变量，只要单纯的重复次数
+#define pureLoop(...) \
+    loopBy( uniqueVarName(macroLoopTempVar), __VA_ARGS__)
+
+// 合并两个 Looping
+#define loop(p1, ...)                  \
+    select0_1(pureLoop(p1),            \
+              loopBy(p1, __VA_ARGS__), \
+              __VA_ARGS__)
+
+//
+#define loopBy2d(var1, times1, var2, ...) \
+    loopBy(var2, times1){                 \
+        loopBy(var2, __VA_ARGS__)
+
+//
+#define pureLoop2d(n, ...) \
+    pureLoop(n){         \
+        pureLoop(__VA_ARGS__)
+
+//
+#define loop2d(p1, p2, ...)                   \
+    select0_2(pureLoop2d(p1, p2),             \
+              pureLoop2d(p1, p2, __VA_ARGS__, \
+              loopBy2d(p1, p2, __VA_ARGS__))  \
+              __VA_ARGS__)
+
---
+++ MacroLib/MacroLib/ForLoopMacro.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "SupportingMacro.h"
+
+// 简化版 for loop
+#define easyFor(var, start, end) \
+    for(int var = start; var <= end; var++)
+
+// 較灵活的 for loop
+#define flexFor(var, start, end, ...) \
+    for(int var = start; var <= end; __VA_ARGS__)
+
+// 合并以上两个 for loop
+#define ffor(var, start, end, ...)                   \
+    select0_1(easyFor(var, start, end),              \
+              flexFor(var, start, end, __VA_ARGS__), \
+              __VA_ARGS__)
+
+
