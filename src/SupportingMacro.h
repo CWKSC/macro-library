e5640f2c167f89b1fda56c0d92449e71a20450a8
Chu Wing Kin <cwksc.person@gmail.com>
2019-08-19 21:28:42 +0800

    Update src file (a part of)

---
+++ src/SupportingMacro.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#define ln putchar('\n')
+#define ln2 printf("\n\n")
+
+#define refer *
+
+#define leftBracket (
+#define rightBracket )
+
+#define leftCurlyBracket {
+#define rightCurlyBracket }
+
+#define comma ,
+
+#define midLayer(...) __VA_ARGS__
+#define eatComma(...) ,##__VA_ARGS__
+
+#define linkMacro_body(a, b) a ## b
+#define linkMacro(a, b) linkMacro_body(a, b)
+#define uniqueVarName(name) linkMacro(name, __COUNTER__)
+#define uniqueVarName2(name) linkMacro_body leftBracket name, __COUNTER__ )
+
+#define get1th(a1, ...) a1
+#define get2th(a1, a2, ...) a2
+#define get3th(a1, a2, a3, ...) a3
+#define get4th(a1, a2, a3, a4, ...) a4
+#define get5th(a1, a2, a3, a4, a5, ...) a5
+#define get6th(a1, a2, a3, a4, a5, a6, ...) a6
+#define get7th(a1, a2, a3, a4, a5, a6, a7, ...) a7
+#define get8th(a1, a2, a3, a4, a5, a6, a7, a8, ...) a8
+#define get9th(a1, a2, a3, a4, a5, a6, a7, a8, a9, ...) a9
+#define get10th(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
+
+#define get1thVS(...) midLayer( get1th(__VA_ARGS__) )
+#define get2thVS(...) midLayer( get2th(__VA_ARGS__) )
+#define get3thVS(...) midLayer( get3th(__VA_ARGS__) )
+#define get4thVS(...) midLayer( get4th(__VA_ARGS__) )
+#define get5thVS(...) midLayer( get5th(__VA_ARGS__) )
+#define get6thVS(...) midLayer( get6th(__VA_ARGS__) )
+#define get7thVS(...) midLayer( get7th(__VA_ARGS__) )
+#define get8thVS(...) midLayer( get8th(__VA_ARGS__) )
+#define get9thVS(...) midLayer( get9th(__VA_ARGS__) )
+#define get10thVS(...) midLayer( get10th(__VA_ARGS__) )
+
+#define parameterNum(...) get10thVS(__VA_ARGS__, 9, 8, 7, 6 , 5, 4, 3, 2, 1)
+
+#define select0_1(m0, m1, ...) get3th leftBracket eatComma(__VA_ARGS__), m1, m0 )
+#define select0_2(m0, m1, m2, ...) get4th leftBracket eatComma(__VA_ARGS__), m2, m1, m0)
+#define select0_3(m0, m1, m2, m3, ...) get5th leftBracket eatComma(__VA_ARGS__), m3, m2, m1, m0)
+#define select0_4(m0, m1, m2, m3, m4, ...) get6th leftBracket eatComma(__VA_ARGS__), m4, m3, m2, m1, m0)
+#define select0_5(m0, m1, m2, m3, m4, m5, ...) get7th leftBracket eatComma(__VA_ARGS__), m5, m4, m3, m2, m1, m0)
+#define select0_6(m0, m1, m2, m3, m4, m5, m6, ...) get8th leftBracket eatComma(__VA_ARGS__), m6, m5, m4, m3, m2, m1, m0)
+#define select0_7(m0, m1, m2, m3, m4, m5, m6, m7, ...) get9th leftBracket eatComma(__VA_ARGS__), m7, m6, m5, m4, m3, m2, m1, m0)
+#define select0_8(m0, m1, m2, m3, m4, m5, m6, m7, m8, ...) get10th leftBracket eatComma(__VA_ARGS__), m8, m7, m6, m5, m4, m3, m2, m1, m0)
+
+#define select0_1VS(va_args, m0, m1) select_va_args_0_1(m1, m0, va_args)
+#define select0_2VS(va_args, m0, m1, m2) select_va_args_0_2(m2, m1, m0, va_args)
+#define select0_3VS(va_args, m0, m1, m2, m3) select_va_args_0_3(m3, m2, m1, m0, va_args)
+#define select0_4VS(va_args, m0, m1, m2, m3, m4) select_va_args_0_4(m4, m3, m2, m1, m0, va_args)
+#define select0_5VS(va_args, m0, m1, m2, m3, m4, m5) select_va_args_0_5(m5, m4, m3, m2, m1, m0, va_args)
+#define select0_6VS(va_args, m0, m1, m2, m3, m4, m5, m6) select_va_args_0_6(m6, m5, m4, m3, m2, m1, m0, va_args)
+#define select0_7VS(va_args, m0, m1, m2, m3, m4, m5, m6, m7) select_va_args_0_7(m7, m6, m5, m4, m3, m2, m1, m0, va_args)
+#define select0_8VS(va_args, m0, m1, m2, m3, m4, m5, m6, m7, m8) select_va_args_0_8(m8, m7, m6, m5, m4, m3, m2, m1, m0, va_args)
+
---
+++ src/main.c
@@ -0,0 +1,120 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdarg.h>
+
+#include "SupportingMacro.h"
+#include "ForLoopMacro.h"
+#include "ArrayLoopingMacro.h"
+#include "LinkedList.h"
+
+int main() {
+
+	// easyFor() //
+	// 1 至 100 的总和 //
+	printf("[easyFor] 1 至 100 的总和\n");
+	int sum_easyFor = 0;
+	easyFor(i, 1, 100) {  // for(int i = 1; i <= 100; i++)
+		sum_easyFor += i;
+	}
+	printf("%d\n", sum_easyFor);
+	ln;
+
+	// flexFor() //
+	// 1 至 100 内奇数的总和 //
+	printf("[flexFor] 1 至 100 内奇数的总和\n");
+	int sum_flexFor = 0;
+	flexFor(i, 1, 100, i += 2) {  // for(int i = 1; i <= 100; i+=2)
+		sum_flexFor += i;
+	}
+	printf("%d\n", sum_flexFor);
+	ln;
+
+	// ffor() //
+	// 计算 [1 至 100] 及 [101 至 200 内奇数] 的总和 //
+	printf("[ffor] 计算 [1 至 100] 及 [101 至 200 内奇数] 的总和\n");
+	int sum_ffor = 0;
+	ffor(i, 1, 100) {  //easyFor(i, 1, 100)
+		sum_ffor += i;
+	}
+	ffor(i, 101, 200, i += 2) {  //flexFor(i, 101, 200, i+=2)
+		sum_ffor += i;
+	}
+	printf("%d\n", sum_ffor);  //12550
+	ln;
+
+	// loopBy() //
+	// 遍历 x * y 的二维阵列 //
+	printf("[loopBy] 遍历 x * y 的二维阵列\n");
+#define x 3
+#define y 4
+	int array2d_loopBy[x][y] = { 0 };
+	loopBy(i, x) {  // for(int i = 0; i != x; i++)
+		loopBy(j, y) {  // for(int j = 0; j != x ; j++)
+			printf("%d", array2d_loopBy[i][j]);
+		}
+		ln;
+	}
+#undef x
+#undef y
+	ln;
+
+	// pureLoop() //
+	// 输出 n * n 的 * 方阵 //
+	printf("[pureLoop] 输出 n * n 的 * 方阵\n");
+#define n 3
+	pureLoop(n) {
+		pureLoop(n) {
+			printf("* ");
+		}
+		ln;
+	}
+#undef n
+	ln;
+
+	// loop2d() //
+	// 遍历 x * y 的二维阵列 //
+	printf("[loop2d] 遍历 x * y 的二维阵列\n");
+#define x 3
+#define y 4
+	int array2d_loop2d[x][y] = { 0 };
+	loop2d(i, x, j, y) {
+		printf("%d", array2d_loop2d[i][j]);
+		rightCurlyBracket
+	ln;
+	}
+	ln;
+#undef x
+#undef y
+
+	// loop2d() //
+	// 二维纯粹循环 //
+	// 输出 n * n 的 * 方阵 //
+	printf("[loop2d] 输出 n * n 的 * 方阵 \n");
+#define n 3
+	loop2d(n, n) {
+		printf("* ");
+		rightCurlyBracket
+	ln;
+	}
+#undef n
+	ln;
+
+	// Linked List //
+	printf("[Linked List]\n");
+	createLinkedList(list);
+	pushFrontByValue(list, 10);
+	pushFrontByValue(list, 12);
+	printLinkedList(list);
+	freeLinkedList(list);
+	ln2;
+
+	int arr[10] = { 1, 3, 4, 2, 1, 2, 3, 7, 9, 1 };
+	forech(int, x, arr,
+		printf("%d ", x);
+	)
+	ln;
+
+	system("pause");
+	return 0;
+}
